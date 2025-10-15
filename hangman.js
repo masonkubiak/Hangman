bd01d525db027fd8a153e6aa7969d2b9b3ed3e59
Mason Kubiak <mayakubiak10@gmail.com>
2025-10-15 10:42:13 -0400

    Code to choose random word from list

---
+++ hangman.js
@@ -0,0 +1,3 @@
+var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+var randomIndex = parseInt(Math.random()*(POSSIBLE_WORDS.length));
+var word = POSSIBLE_WORDS[randomIndex];
