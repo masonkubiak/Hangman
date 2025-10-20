40376280fd7fd9aeaa7b6f34e746e0f9fc13b78b
Mason Kubiak <mayakubiak10@gmail.com>
2025-10-20 10:33:56 -0400

    Added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -1,15 +1,34 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
-var randomIndex = parseInt(Math.random()*(POSSIBLE_WORDS.length));
+var randomIndex = parseInt(Math.random() * (POSSIBLE_WORDS.length));
 var word = "";
+var guesses = "";
 
-function newGame(){
-    var randomIndex = parseInt(Math.random()*(POSSIBLE_WORDS.length));
+function newGame() {
+    var randomIndex = parseInt(Math.random() * (POSSIBLE_WORDS.length));
     word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    updatePage();
+}
 
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+    updatePage();
+}
+
+function updatePage() {
     var clueString = "";
-    for(var i = 0; i<word.length; i++){
-        clueString+="_ ";
+    for (var i = 0; i < word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        } else {
+            clueString += "_ ";
+        }
     }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed letters: " + guesses;
 }
