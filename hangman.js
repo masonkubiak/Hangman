3e5edf0ec8b9ed84f83b5f8c58fd5f0860b2e4a5
Mason Kubiak <mayakubiak10@gmail.com>
2025-10-22 09:53:49 -0400

    Added image functionality

--- hangman.js
+++ hangman.js
@@ -1,4 +1,6 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+var MAX_GUESSES = 6;
+var guess_count = MAX_GUESSES;
 var randomIndex = parseInt(Math.random() * (POSSIBLE_WORDS.length));
 var word = "";
 var guesses = "";
@@ -7,12 +9,16 @@ function newGame() {
     var randomIndex = parseInt(Math.random() * (POSSIBLE_WORDS.length));
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
+    guess_count = MAX_GUESSES;
     updatePage();
 }
 
 function guessLetter() {
     var input = document.getElementById("guess");
     var letter = input.value;
+    if (word.indexOf(letter) < 0) {
+        guess_count--;
+    }
     guesses += letter;
     updatePage();
 }
@@ -27,8 +33,13 @@ function updatePage() {
             clueString += "_ ";
         }
     }
+    // update clue string
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+    //update guesses from user
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed letters: " + guesses;
+    //update the image
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + ".gif";
 }
